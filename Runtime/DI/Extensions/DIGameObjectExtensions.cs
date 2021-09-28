cdccd3b12a5e3a126de1f23cc74aabc09ca819f7
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-09-28 20:31:44 +0200

    Di extensions

---
+++ Runtime/DI/Extensions/DIGameObjectExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Juce.Core.DI.Builder;
+using Juce.Core.DI.Container;
+using System;
+using UnityEngine;
+
+namespace JuceUnity.Core.DI.Extensions
+{
+    public static class DIGameObjectExtensions
+    {
+        public static IDIBindingActionBuilder<T> FromGameObject<T>(
+            this IDIBindingBuilder<T> builder, 
+            GameObject gameObject
+            ) where T : MonoBehaviour
+        {
+            Func<IDIResolveContainer, T> function = (IDIResolveContainer resolver) =>
+            {
+                Type type = typeof(T);
+
+                if(gameObject == null)
+                {
+                    throw new Exception($"Tried to bind {type.Name} from {nameof(GameObject)}, " +
+                        $"but {nameof(GameObject)} was null");
+                }
+
+                T foundObject = gameObject.GetComponent<T>();
+
+                if(foundObject == null)
+                {
+                    throw new Exception($"Tried to bind {type.Name} from {nameof(GameObject)}, " +
+                        $"but the {nameof(MonoBehaviour)} could not be found");
+                }
+
+                return foundObject;
+            };
+
+            return builder.FromFunction(function);
+        }
+
+        public static IDIBindingActionBuilder<T> FromGameObjectInChildren<T>(
+            this IDIBindingBuilder<T> builder,
+            GameObject gameObject
+            ) where T : MonoBehaviour
+        {
+            Func<IDIResolveContainer, T> function = (IDIResolveContainer resolver) =>
+            {
+                Type type = typeof(T);
+
+                if (gameObject == null)
+                {
+                    throw new Exception($"Tried to bind {type.Name} from {nameof(GameObject)}, " +
+                        $"but {nameof(GameObject)} was null");
+                }
+
+                T foundObject = gameObject.GetComponentInChildren<T>();
+
+                if (foundObject == null)
+                {
+                    throw new Exception($"Tried to bind {type.Name} from {nameof(GameObject)}, " +
+                        $"but the {nameof(MonoBehaviour)} could not be found");
+                }
+
+                return foundObject;
+            };
+
+            return builder.FromFunction(function);
+        }
+    }
+}
