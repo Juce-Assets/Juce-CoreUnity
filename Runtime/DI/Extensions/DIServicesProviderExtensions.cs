46e4b20ae3964511bd44bea62a4d4eda6bbdec00
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-09-29 01:19:30 +0200

    Extensions

---
+++ Runtime/DI/Extensions/DIServicesProviderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Juce.Core.DI.Builder;
+using Juce.Core.DI.Container;
+using Juce.CoreUnity.Service;
+using System;
+
+namespace JuceUnity.Core.DI.Extensions
+{
+    public static class DIServicesProviderExtensions
+    {
+        public static IDIBindingActionBuilder<T> FromServicesProvider<T>(this IDIBindingBuilder<T> builder) where T : IService
+        {
+            Func<IDIResolveContainer, T> function = (IDIResolveContainer resolver) =>
+            {
+                Type type = typeof(T);
+
+                bool found = ServicesProvider.TryGetService(out T service);
+
+                if (!found)
+                {
+                    throw new Exception($"Tried to bind {type.Name} from {nameof(ServicesProvider)}, " +
+                        $"but it could not be found");
+                }
+
+                return service;
+            };
+
+            return builder.FromFunction(function);
+        }
+    }
+}
