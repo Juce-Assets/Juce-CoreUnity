6985e0dd0ef26265733661a879f9e204995c89bb
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-01-12 20:38:34 +0100

    await

---
+++ Runtime/Extensions/JuceCoreUnityPlaybleDirectorExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.Playables;
+
+public static class JuceCoreUnityPlaybleDirectorExtensions
+{
+    public static Task Play(this PlayableDirector component, CancellationToken cancellationToken)
+    {
+        component.Play();
+
+        return AwaitCompletition(component, cancellationToken);
+    }
+
+    public static Task Play(this PlayableDirector component, PlayableAsset playableAsset, CancellationToken cancellationToken)
+    {
+        component.Play(playableAsset);
+
+        return AwaitCompletition(component, cancellationToken);
+    }
+
+    public static Task AwaitCompletition(this PlayableDirector component, CancellationToken cancellationToken)
+    {
+        if (component.state != PlayState.Playing)
+        {
+            return Task.CompletedTask;
+        }
+
+        TaskCompletionSource<object> taskCompletionSource = new TaskCompletionSource<object>();
+
+        Action<PlayableDirector> onStoped = (PlayableDirector _) =>
+        {
+            taskCompletionSource.TrySetResult(default);
+        };
+
+        component.stopped += onStoped;
+
+        cancellationToken.Register(() => onStoped?.Invoke(component));
+
+        return taskCompletionSource.Task;
+    }
+}
