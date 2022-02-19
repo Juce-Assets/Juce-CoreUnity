30df7cf647ef59df46a3f0a151420e0b1df48936
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-02-19 16:26:15 +0100

    Bootstrap

---
+++ Scripts/Runtime/Bootstraps/Bootstrap.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Juce.CoreUnity.Bootstraps
+{
+    public abstract class Bootstrap : MonoBehaviour
+    {
+        protected void Awake()
+        {
+            Run().RunAsync();
+        }
+
+        protected abstract Task Run();
+    }
+}
