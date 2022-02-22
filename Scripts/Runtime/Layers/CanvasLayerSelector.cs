5139a706da46fd10b6877e6dbe19fb9ee00dac53
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-02-22 23:45:31 +0100

    Layers

---
+++ Scripts/Runtime/Layers/CanvasLayerSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Juce.CoreUnity.Layers
+{
+    public class CanvasLayerSelector : MonoBehaviour
+    {
+        [Header("Layer")]
+        [SerializeField] private Layer layer = default;
+
+        [Header("Target")]
+        [SerializeField] private Canvas target = default;
+
+        private void Awake()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            target.sortingOrder = layer.LayerValue;
+        }
+    }
+}
---
+++ Scripts/Runtime/Layers/RendererLayerSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Juce.CoreUnity.Layers
+{
+    public class RendererLayerSelector : MonoBehaviour
+    {
+        [Header("Layer")]
+        [SerializeField] private Layer layer = default;
+
+        [Header("Target")]
+        [SerializeField] private Renderer target = default;
+
+        private void Awake()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            target.sortingOrder = layer.LayerValue;
+        }
+    }
+}
