f1cce4b27b747430491714ed127e4383577e429f
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-02-24 20:42:12 +0100

    Visibles

---
+++ Scripts/Runtime/Visibles/TweenPlayerAnimationVisible.cs
@@ -0,0 +1,32 @@
+﻿using Juce.Core.Visibility;
+using Juce.CoreUnity.TweenComponent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Juce.CoreUnity.Visibles
+{
+    public class TweenPlayerAnimationVisible : IVisible
+    {
+        private readonly TweenPlayerAnimation showAnimation;
+        private readonly TweenPlayerAnimation hideAnimation;
+
+        public TweenPlayerAnimationVisible(
+            TweenPlayerAnimation showAnimation,
+            TweenPlayerAnimation hideAnimation
+            )
+        {
+            this.showAnimation = showAnimation;
+            this.hideAnimation = hideAnimation;
+        }
+
+        public Task SetVisible(bool visible, bool instantly, CancellationToken cancellationToken)
+        {
+            if(visible)
+            {
+                return showAnimation.Execute(instantly, cancellationToken);
+            }
+
+            return hideAnimation.Execute(instantly, cancellationToken);
+        }
+    }
+}
