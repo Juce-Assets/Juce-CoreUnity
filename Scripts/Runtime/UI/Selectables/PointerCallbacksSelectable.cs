d517e1e4cd4943f9e805ecabb0b8a4432999e85f
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-02 18:19:39 +0100

    Working on selectables

---
+++ Scripts/Runtime/UI/Selectables/PointerCallbacksSelectable.cs
@@ -0,0 +1,26 @@
+﻿using Juce.CoreUnity.PointerCallback;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Juce.CoreUnity.Ui
+{
+    public class PointerCallbacksSelectable : MonoBehaviour, ISelectHandler, IDeselectHandler, ISubmitHandler
+    {
+        [SerializeField] private PointerCallbacks pointerCallbacks = default;
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            pointerCallbacks.OnPointerEnter(new PointerEventData(EventSystem.current));
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            pointerCallbacks.OnPointerExit(new PointerEventData(EventSystem.current));
+        }
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            pointerCallbacks.OnPointerClick(new PointerEventData(EventSystem.current));
+        }
+    }
+}
