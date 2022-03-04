cf068a8eeb6c66bb886cfe941355ba21c18a33d1
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-04 14:48:47 +0100

    Application

---
+++ Scripts/Runtime/Application/JuceAppliaction.cs
@@ -0,0 +1,14 @@
+﻿namespace Juce.CoreUnity
+{
+    public static class JuceAppliaction
+    {
+        public static void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
