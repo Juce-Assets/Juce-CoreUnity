acd4ef9fc834da32f51c9ea738b34ec96acfca63
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-09 15:40:41 +0100

    Improved timers

---
+++ Scripts/Runtime/Time/Timers/ScaledUnityTimer.cs
@@ -0,0 +1,14 @@
+﻿using Juce.Core.Time;
+using System;
+
+namespace Juce.CoreUnity.Time
+{
+    public class ScaledUnityTimer : CallbackTimer
+    {
+        public ScaledUnityTimer()
+            : base(() => TimeSpan.FromSeconds(UnityEngine.Time.time))
+        {
+
+        }
+    }
+}
--- Scripts/Runtime/Time/Timers/UnscaledUnityTimer.cs
+++ Scripts/Runtime/Time/Timers/UnscaledUnityTimer.cs
@@ -1,75 +1,14 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Juce.Core.Time;
+using System;
 
 namespace Juce.CoreUnity.Time
 {
-    public class UnscaledUnityTimer : IUnityTimer
+    public class UnscaledUnityTimer : CallbackTimer
     {
-        private bool started;
-        private TimeSpan startTime;
-
-        public TimeSpan Time
+        public UnscaledUnityTimer() 
+            : base(() => TimeSpan.FromSeconds(UnityEngine.Time.unscaledTime))
         {
-            get
-            {
-                if (!started)
-                {
-                    return TimeSpan.Zero;
-                }
-
-                return TimeSpan.FromSeconds(UnityEngine.Time.unscaledTime) - startTime;
-            }
-        }
-
-        public void Start()
-        {
-            if (started)
-            {
-                return;
-            }
-
-            started = true;
-
-            startTime = TimeSpan.FromSeconds(UnityEngine.Time.unscaledTime);
-        }
-
-        public void Reset()
-        {
-            started = false;
-
-            startTime = TimeSpan.Zero;
-        }
-
-        public void Restart()
-        {
-            Reset();
-            Start();
-        }
-
-        public bool HasReached(TimeSpan time)
-        {
-            if(!started)
-            {
-                return false;
-            }
-
-            return Time >= time;
-        }
-
-        public async Task AwaitReach(TimeSpan time, CancellationToken cancellationToken)
-        {
-            while(!HasReached(time) && !cancellationToken.IsCancellationRequested)
-            {
-                await Task.Yield();
-            }
-        }
-
-        public Task AwaitTime(TimeSpan time, CancellationToken cancellationToken)
-        {
-            TimeSpan timeToReach = Time + time;
-
-            return AwaitReach(timeToReach, cancellationToken);
+           
         }
     }
 }
