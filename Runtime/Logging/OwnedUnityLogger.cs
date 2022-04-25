5cbfac101bba02096c82285207e0c21532ac9049
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-04-25 19:39:31 +0200

    Logging

---
+++ Runtime/Logging/OwnedUnityLogger.cs
@@ -0,0 +1,21 @@
+﻿using Juce.Core.Logging;
+
+namespace Juce.CoreUnity.Logging
+{
+    public class OwnedUnityLogger : ILoggerOutput
+    {
+        private readonly ILoggerOwner owner;
+
+        public OwnedUnityLogger(ILoggerOwner owner)
+        {
+            this.owner = owner;
+        }
+
+        public void Output(string output)
+        {
+            string finalString = string.Format("[{0}] {1}", owner.Name, output);
+
+            UnityEngine.Debug.Log(finalString);
+        }
+    }
+}
---
+++ Runtime/Logging/UnityLogger.cs
@@ -0,0 +1,19 @@
+﻿using Juce.Core.Logging;
+
+namespace Juce.CoreUnity.Logging
+{
+    public class UnityLogger : ILoggerOutput
+    {
+        public static readonly UnityLogger Instance = new UnityLogger();
+
+        private UnityLogger()
+        {
+            
+        }
+
+        public void Output(string output)
+        {
+            UnityEngine.Debug.Log(output);
+        }
+    }
+}
