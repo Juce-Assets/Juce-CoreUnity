901b2ab624f1bbaa41f609e0f680517ac583bff0
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-06-02 22:22:07 +0200

    Merged persistense

---
+++ Runtime/Persistense/Serialization/SerializableData.cs
@@ -0,0 +1,102 @@
+using Juce.CoreUnity.Files;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Juce.Persistence.Serialization
+{
+    public class SerializableData<T> where T : class
+    {
+        private readonly string localPath;
+
+        public event Action<T> OnSave;
+        public event Action<T, bool> OnLoad;
+
+        public T Data { get; private set; }
+
+        public SerializableData(string localPath)
+        {
+            this.localPath = localPath;
+        }
+
+        public async Task Save(CancellationToken cancellationToken)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            settings.Formatting = Formatting.Indented;
+
+            TryGenerateData();
+
+            OnSave?.Invoke(Data);
+
+            try
+            {
+                string finalPath = SerializableDataUtils.GetPersistenceDataFilepath(localPath);
+
+                string dataString = JsonConvert.SerializeObject(Data, settings);
+                byte[] dataBytes = Encoding.UTF8.GetBytes(dataString);
+
+                await FileUtils.SaveBytesAsync(finalPath, dataBytes, cancellationToken);
+
+                UnityEngine.Debug.Log($"{nameof(SerializableData<T>)} {typeof(T).Name} saved \n {Data}");
+            }
+            catch (Exception exception)
+            {
+                UnityEngine.Debug.LogError($"Error saving {nameof(SerializableData<T>)} {typeof(T).Name} with " +
+                    $"the following exception {exception}");
+            }
+        }
+
+        public async Task Load(CancellationToken cancellationToken)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            settings.Formatting = Formatting.Indented;
+
+            string finalPath = SerializableDataUtils.GetPersistenceDataFilepath(localPath);
+
+            try
+            {
+                byte[] bytes = await FileUtils.LoadBytesAsync(finalPath, cancellationToken);
+
+                if (bytes != null)
+                {
+                    string result = Encoding.UTF8.GetString(bytes);
+
+                    Data = JsonConvert.DeserializeObject<T>(result);
+
+                    OnLoad?.Invoke(Data, false);
+
+                    UnityEngine.Debug.Log($"{nameof(SerializableData<T>)} {typeof(T).Name} loaded \n {Data}");
+                }
+                else
+                {
+                    TryGenerateData();
+
+                    OnLoad?.Invoke(Data, true);
+
+                    UnityEngine.Debug.Log($"{nameof(SerializableData<T>)} {typeof(T).Name} not found. Creating with default values");
+                }
+            }
+            catch (Exception exception)
+            {
+                UnityEngine.Debug.LogError($"Error loading {nameof(SerializableData<T>)} {typeof(T).Name} " +
+                    $"with the following exception {exception}");
+            }
+
+            TryGenerateData();
+        }
+
+        private void TryGenerateData()
+        {
+            if (Data != null)
+            {
+                return;
+            }
+
+            Data = Activator.CreateInstance<T>();
+        }
+    }
+}
---
+++ Runtime/Persistense/Serialization/SerializableDataUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Juce.Persistence.Serialization
+{
+    public static class SerializableDataUtils
+    {
+        public static string GetPersistenceDataFilepath(string fileName)
+        {
+            return string.Format(
+                "{0}{1}",
+                GetPersistenceDataFolder(),
+                fileName
+                );
+        }
+
+        public static string GetPersistenceDataFolder()
+        {
+            return Path.Combine(
+                Application.persistentDataPath,
+                string.Format("PersistenceData{0}", Path.DirectorySeparatorChar)
+                );
+        }
+
+        public static void ClearPersistanceData()
+        {
+            string path = GetPersistenceDataFolder();
+
+            Directory.Delete(path, recursive: true);
+        }
+    }
+}
