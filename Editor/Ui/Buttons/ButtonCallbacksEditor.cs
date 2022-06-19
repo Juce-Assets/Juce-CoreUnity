9dfe641134a7cd31520b267b04aa583c583065bb
DESKTOP-OH1HMN0\guill <gsunyercaldu@gmail.com>
2022-06-19 19:18:30 +0200

    Custom editor

---
+++ Editor/Ui/Buttons/ButtonCallbacksEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+
+namespace Juce.CoreUnity.Ui.Buttons
+{
+    [CustomEditor(typeof(ButtonCallbacks))]
+    public class ButtonCallbacksEditor : Editor
+    {
+        private SerializedProperty triggerPointerUpOnPointerExitProperty;
+        private SerializedProperty navigationProperty;
+
+        private void OnEnable()
+        {
+            GatherSerializedProperties();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(triggerPointerUpOnPointerExitProperty);
+            EditorGUILayout.PropertyField(navigationProperty);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void GatherSerializedProperties()
+        {
+            triggerPointerUpOnPointerExitProperty = serializedObject.FindProperty("triggerPointerUpOnPointerExit");
+            navigationProperty = serializedObject.FindProperty("m_Navigation");
+        }
+    }
+}
